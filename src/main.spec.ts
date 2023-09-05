85d6e68e983e19f6ede198712c48e9738d542f79
Junior Motta <mottajunior5@gmail.com>
2023-09-05 20:05:04 -0300

    add commented

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,3 +1,18 @@
+/*
+Step By Step em desafios de codigo:
+  Entender a natureza do Problema
+  Eventualmente vou conseguir entender a natureza das solucoes possiveis de se encontrar
+  E quais sao os mecanismos que vou utilizar para transformar o problema na solucao possivel
+  Problema de busca? Problema de otimizacao? Problema de precisao?
+
+annotations:
+  se for um problema classe P (possivel resolver em tempo polimonial) [buscar um endereco na lista telefonica]
+  se for um problema classe NP (possivel verificar em tempo polimonial) [logica de um jogo sudoku]
+  se for um problema classe NP-HARD (super complexo e nao possivel resolver com algum algoritmo que se encaixe nas classes acima (P,NP)) [logica de um jogo de xadrez]
+*/
+
+
+
 describe('Main flow applying different algorithms and structured data types', () => {
   describe('root', () => {
     it('should return "Hello World!"', () => {
